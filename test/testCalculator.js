6a9dac417cfc1ade6a23f7f385409063dba4de75
phaneendra <phaneendraandukuri@gmail.com>
2020-03-13 18:04:54 +0530

    modified test for sum function

--- test/testCalculator.js
+++ test/testCalculator.js
@@ -4,7 +4,7 @@ const sum = function(a, b) {
   return a + b;
 };
 
-describe('', function() {
+describe('sum', function() {
   it('should give the sum of two positive numbers', function() {
     assert.equal(sum(1, 2), 3);
   });
